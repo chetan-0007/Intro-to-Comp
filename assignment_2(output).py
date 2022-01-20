3394b1d20740420434cda51afe2ed129d50dd79f
chetan-0007 <97439606+chetan-0007@users.noreply.github.com>
2022-01-20 23:44:13 +0530

    Add files via upload

---
+++ assignment_2(output).py
@@ -0,0 +1,52 @@
+Question 1
+a) 35
+b) egaugnal evitisnes esac a si nohtyP
+c) a case sensitive
+d) Python is object oriented language
+e) 10
+f) Pythonisacasesensitivelanguage
+
+
+Question 2
+Hey Chetan here! 
+My SID is 21103059 
+I am from CSE department and my CGPA is 9.9
+
+
+Question 3
+For a=56 and b=10
+a) a&b =  8
+b) a|b =  58
+c) a^b =  50
+d) a<<2 =  224
+   b<<2 =  40
+e) a>>2 =  14
+   b>>4 =  0
+
+
+Question 4
+Enter first number:
+22
+Enter second number:
+58
+Enter third number:
+12
+The greatest number is:  58
+
+
+Question 5
+To check if 'name' is present in string enetered by user
+Enter any text:
+Hello, my name is Chetan
+YES
+
+
+Question 6
+To check if it is poosible to form triangle by lenth of three sides given by user
+Enter length of first side (in cm):
+12
+Enter length of second side (in cm):
+8
+Enter length of third side (in cm):
+21
+NO
