f0d1caeedce8e00030f263922ddd8984486652e3
chetan-0007 <97439606+chetan-0007@users.noreply.github.com>
2022-02-08 20:58:04 +0530

    Add files via upload

---
+++ Assignment_3 (output).py
@@ -0,0 +1,104 @@
+SOLUTION 1
+
+Enter text:
+Hello my name is Chetan. I am from CSE branch. My SID is 21103059
+
+Reqired dictionary with each word of string as key and its's repitation as value:
+ {'hello': 1, 'my': 2, 'name': 1, 'is': 2, 'chetan.': 1, 'i': 1, 'am': 1, 'from': 1, 'cse': 1, 'branch.': 1, 'sid': 1, '21103059': 1}
+
+SOLUTION 2
+
+Enter year: 2020
+Enter month: 2
+Enter day: 29
+Next date in the format(dd/mm/yyyy) is: 1/3/2020 
+
+SOLUTION 3
+
+How many terms to be entered in list?:
+5
+Enter required element:
+12
+Enter required element:
+8
+Enter required element:
+6
+Enter required element:
+10
+Enter required element:
+3
+
+The required list is:
+ [(12, 144), (8, 64), (6, 36), (10, 100), (3, 9)]
+
+SOLUTION 4
+
+Enter your grade point: 8
+Your grade is 'B+' and Very Good performance
+
+SOLUTION 5
+
+ ABCDEFGHIJK
+  ABCDEFGHI
+   ABCDEFG
+    ABCDE
+     ABC
+      A
+
+SOLUTION 6
+
+Enter name: Chetan
+Enter SID: 21103059
+{'Chetan': 21103059}
+
+Want to make another entry?
+ Press 'Y' for YES / 'N' for NO.
+Y
+
+Enter name: ABC
+Enter SID: 21103099
+{'Chetan': 21103059, 'ABC': 21103099}
+
+Want to make another entry?
+ Press 'Y' for YES / 'N' for NO.
+Y
+
+Enter name: XYZ
+Enter SID: 21103011
+{'Chetan': 21103059, 'ABC': 21103099, 'XYZ': 21103011}
+
+Want to make another entry?
+ Press 'Y' for YES / 'N' for NO.
+N
+
+a) The required dictionary is
+ {'Chetan': 21103059, 'ABC': 21103099, 'XYZ': 21103011}
+
+b) Dictionary sorted on the basis of names:
+ {'ABC': 21103099, 'Chetan': 21103059, 'XYZ': 21103011}
+
+c) Dictionary sorted on the basis of SID:
+ {'XYZ': 21103011, 'Chetan': 21103059, 'ABC': 21103099}
+
+d) Enter a SID to get it's name:21103059
+Name related to following sid is: 
+ Chetan
+
+SOLUTION 7
+
+How many terms to be printed in fibonacci series?
+7
+
+Required Fibonacci series is:
+ [0, 1, 1, 2, 3, 5, 8]
+
+Average of following series is:
+ 2.857142857142857
+
+SOLUTION 8
+
+a) {1, 3, 5, 6, 8}
+b) {17, 3, 6, 8, 9, 13}
+c) {1, 2, 4, 5}
+d) {6, 7, 8, 9, 10}
+e) {10, 7}
